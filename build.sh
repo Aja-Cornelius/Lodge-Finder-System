27a852fe82d10ab1eb98260db3f722115b650b61
Aja-Cornelius <114728143+Aja-Cornelius@users.noreply.github.com>
2026-07-08 14:08:16 +0100

    config: add build command, update whitenoise version for Vercel deploy

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo '==> Running database migrations...'
+python manage.py migrate --noinput
+echo '==> Collecting static files...'
+python manage.py collectstatic --noinput
+echo '==> Build complete!'
