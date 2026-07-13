f047504bb58c0264d2f1ca32b0c8380b50e7b145
Aja-Cornelius <114728143+Aja-Cornelius@users.noreply.github.com>
2026-07-13 17:32:05 +0100

    fix: initialize django before migration call in wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,14 +8,18 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
 
 # Execute database migrations when the serverless function spins up
 from django.core.management import call_command
 try:
+    print("Executing migrations at runtime startup...")
     call_command('migrate', interactive=False)
+    print("Migrations executed successfully!")
 except Exception as e:
     print(f"Error running migration at startup: {e}")
 
